c960313d29975549ab06837b9d8b8921141e8907
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-07-12 10:54:23 +0300

    font-size tweak.

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 const Container = styled.div`
   color: white;
 
-  font-size: 1.8em;
+  font-size: 1.65em;
   font-weight: bold;
   max-width: 20ch;
+
+  line-height: 1.4em;
 
   margin: 2.5em auto 0 auto;
   text-align: center;
